cdd43c5c02bd39c596d7730743b212ed0c00f8d5
Rob Saunders <rob@wearebrandnew.com>
2019-10-04 12:31:22 +0800

    basic actix webserver

--- templates/rust/src/main.rs
+++ templates/rust/src/main.rs
@@ -1,3 +1,25 @@
+use actix_web::{http::Method, server, App, HttpRequest, Path, Responder};
+
+fn index(_req: HttpRequest) -> impl Responder {
+    "Hello from the index page"
+}
+
+fn hello(path: Path<String>) -> impl Responder {
+    format!("Hello {}!", *path)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or("8000".to_string())
+        .parse::<u16>()
+        .unwrap_or(8000);
+
+    server::new(|| {
+        vec![App::new()
+            .resource("/", |r| r.method(Method::GET).with(index))
+            .resource("/hello/{name}", |r| r.method(Method::GET).with(hello))]
+    })
+    .bind(format!("127.0.0.1:{}", port))
+    .expect(&*format!("Can not bind to port {}", port))
+    .run();
 }
