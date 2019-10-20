34207bc1bba29d280b79e40b3e16c44c06f5c8a8
Alexei Rudakov <rudakov3000@mail.ru>
2019-10-20 17:31:55 +0800

    tt

--- templates/kotlin/API/Models.kt
+++ templates/kotlin/API/Models.kt
@@ -1,8 +1,5 @@
 package com.example.androidtest1.API
 
-
 import kotlinx.serialization.json.*
 import kotlinx.serialization.*
 
-@Serializable
-data class ModelSample(var name: String)
--- templates/kotlin/API/Requests.kt
+++ templates/kotlin/API/Requests.kt
@@ -3,7 +3,9 @@ package com.example.androidtest1.API
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RequestSample(var name: String) {//: APIRequest {
-//    override val method = Method.post
-//    override val urlPath = "ololo"
+data class RequestSample(var sss: String): APIRequest {
+    override val method: Method = Method.post
+    override val path : String= "ololo"
+    override val response_type: String = "SomeType"
+    override val error_type: String = "SomeErrorType"
 }
