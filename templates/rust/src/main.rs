39825692b460f6d9203d3eeb94be0ac8bdee20d2
Rob Saunders <rob@wearebrandnew.com>
2019-10-30 06:19:18 +0800

    updated default rust example

--- templates/rust/src/main.rs
+++ templates/rust/src/main.rs
@@ -1,25 +1,18 @@
-use actix_web::{http::Method, server, App, HttpRequest, Path, Responder};
+use actix_web::{web, App};
+mod routes;
 
-fn index(_req: HttpRequest) -> impl Responder {
-    "Hello from the index page"
-}
-
-fn hello(path: Path<String>) -> impl Responder {
-    format!("Hello {}!", *path)
-}
-
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let port: u16 = std::env::var("PORT")
         .unwrap_or("8000".to_string())
         .parse::<u16>()
         .unwrap_or(8000);
 
-    server::new(|| {
-        vec![App::new()
-            .resource("/", |r| r.method(Method::GET).with(index))
-            .resource("/hello/{name}", |r| r.method(Method::GET).with(hello))]
+    actix_web::HttpServer::new(|| {
+        App::new()
+            .configure(routes::configure)
+            .route("/", web::get().to(routes::root))
     })
     .bind(format!("127.0.0.1:{}", port))
     .expect(&*format!("Can not bind to port {}", port))
-    .run();
+    .run()
 }
