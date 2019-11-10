e01fcdfea4f5ab7a1a8c07fe6a1c5e2ef8ed2952
Alexei Rudakov <rudakov3000@mail.ru>
2019-11-10 20:05:10 +0800

    typescript  template

---
+++ templates/typescript/API/APIBase.ts
@@ -0,0 +1,38 @@
+
+
+
+
+class APIRequest {
+    path(): string {
+        return ""
+    }
+
+    method(): string {
+        return "POST"
+    }
+
+
+    send<T>(callback:(result:T)=>void) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(this.method(), this.path(), true);
+        if (callback) xhr.onload = function() { callback(JSON.parse(this.responseText)); };
+    
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(this));
+    }    
+}
+
+//-------------------------------------------------
+// Simple function to GET or POST
+function httpCall(method: string, url:string, data:any, callback:(result:any)=>any) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    if (callback) xhr.onload = function() { callback(JSON.parse(this['responseText'])); };
+    if (data != null) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    }
+    else xhr.send();
+}
+
+
